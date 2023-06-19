650b7527743c37f8a444b2c6dd0a9c8d7714aab5
veera-bellevue <135533549+veera-bellevue@users.noreply.github.com>
2023-06-19 00:34:54 -0400

    week2 assignments
    Assignment0, Assignment1, Assignment2 done

--- assignments/assignment00/assignment_00_InduguVeera.R
+++ assignments/assignment00/assignment_00_InduguVeera.R
@@ -1,52 +1,56 @@
 # Assignment: ASSIGNMENT 0
-# Name: Lastname, Firstname
-# Date: 2010-02-14
+# Name: Indugu, Veera
+# Date: 2023-06-15
 
 # Basics
 
 ## Add 8 and 5
-___
-
+add_fn <- 8+5
+add_fn
 ## Subtract 6 from 22
-___
+sub_fn <- 22-6
+sub_fn
 
 ## Multiply 6 by 7
-___
+multiply_fn <- 6*7
+multiply_fn
 
 ## Add 4 to 6 and divide the result by 2
-___
-
+compute_add_div_fn <- ((4+6)/2)
+compute_add_div_fn
 ## Compute 5 modulo 2
-___
-
+compute_modulo <- 5%%2
+compute_modulo
 ## Assign the value 82 to the variable x
 ## Print x
-x <- ___
+x <- 82
 x
 
 ## Assign the value 41 to the variable y
 ## Print y
-y <- ___
-___
+y <- 41
+y
 
 ## Assign the output of x + y to the variable z
 ## Print z
-z <- ___
-___
+z <- x+y
+z
 
 ## Assign the string value "DSC520" to the variable class_name
 ## Print the value of class_name
-class_name <- ___
+class_name <- "DSC520"
+class_name
 
 ## Assign the string value of TRUE to the variable is_good
 ## Print the value of is_good
-is_good <- ___
-
+is_good <- TRUE
+is_good
 ## Check the class of the variable is_good using the `class()` function
-___
+class(is_good)
 
 ## Check the class of the variable z using the `class()` function
-___
+class(z)
 
 ## Check the class of the variable class_name using the class() function
-___
+class(class_name)
+
